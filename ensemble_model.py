05bd49472d752adac28568beb6fefea4614d00ad
Sourav Singh <ssouravsingh12@gmail.com>
2018-02-02 20:41:23 +0530

    Update ensemble_model.py

--- ensemble_model.py
+++ ensemble_model.py
@@ -62,27 +62,3 @@ scores = cross_val_score(eclf1, X_new, y, cv=5)
 print 'score:',scores
 average_acc = sum(scores)/len(scores)
 print 'average_score:',average_acc
-
-fpr = dict()
-tpr = dict()
-roc_auc = dict()
-for i in range(n_classes):
-    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
-    roc_auc[i] = auc(fpr[i], tpr[i])
-
-# Compute micro-average ROC curve and ROC area
-fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
-roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-
-plt.figure()
-lw = 2
-plt.plot(fpr[2], tpr[2], color='darkorange',
-         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
-plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic example')
-plt.legend(loc="lower right")
-plt.savefig("output.png")
